f1d8f82de382fd1eff2860af7f3654442309b52d
ebuggs <ebuggs212@gmail.com>
2023-08-23 11:24:15 -0400

    add gameboard and players objects

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const gameBoard = () => {
+  const board = ["", "X", "", "", "", "", "", "", ""];
+
+  return board;
+};
+
+//PLAYERS DEFINED IN FACTORY EXPRESSIONS
+// const player = (name, symbol) => {
+//   const getName = () => name;
+//   const getSymbol = () => symbol;
+
+//   return getName, getSymbol;
+// };
+
+// const players = [player("Player 1", "X"), player("Player 2", "O")];
+
+const players = [
+  { name: "Player 1", symbol: "X" },
+  { name: "Player 2", symbol: "O" },
+];
