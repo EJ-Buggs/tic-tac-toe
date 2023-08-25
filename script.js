b574ad9e77a774c0052fbd81b46becc044dc07a8
ebuggs <ebuggs212@gmail.com>
2023-08-25 09:23:21 -0400

    refactored code with module for gameboard and winning conditions and refactored functions with factory functions

--- script.js
+++ script.js
@@ -1,88 +1,91 @@
-const reset = document.querySelector(".reset");
-const cells = document.querySelectorAll(".cell");
+//1 GAMEBOARD MODULE
+const gameBoard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
 
-//1 GAMEBOARD
-let playing = true;
-let board = ["", "", "", "", "", "", "", "", ""];
+  const getBoard = () => board;
 
-//2 PLAYERS
-const players = [
-  { name: "Player 1", symbol: "X" },
-  { name: "Player 2", symbol: "O" },
-];
-
-//WINNING CONDITIONS
-const winCondition = [
-  //HORIZONTAL
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  //VERTICAL
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  //DIAGONAL
-  [2, 4, 6],
-  [0, 4, 8],
-];
-// Create a function to check for a winner
-function checkForWinner() {
-  for (let i = 0; i < winCondition.length; i++) {
-    const [a, b, c] = winCondition[i];
-    if (board[a] && board[a] == board[b] && board[a] == board[c]) {
-      document.querySelector(
-        ".message"
-      ).textContent = `${currentPlayer.name} WINS!`;
-      reset.classList.remove("hidden");
+  const updateBoard = (index, symbol) => {
+    if (board[index] === "") {
+      board[index] = symbol;
+      return true;
     }
-  }
-  playing = false;
-  return;
-}
+    return false;
+  };
 
-//3 PLAYER START
-let currentPlayer = players[0];
+  const resetBoard = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+    // document.querySelector(".message").classList.add("hidden");
+  };
 
-//RESET GAME
-function newGame() {
-  currentPlayer = players[0];
-  board = ["", "", "", "", "", "", "", "", ""];
-  document.querySelector(".message").textContent = "";
-  reset.classList.add("hidden");
-}
+  return { getBoard, updateBoard, resetBoard };
+})();
 
-reset.addEventListener("click", () => {
-  cells.forEach((cell) => {
-    cell.textContent = "";
-    newGame();
+//PLAYER FACTORY FUNCTION
+const playerFactory = (name, symbol) => {
+  return { name, symbol };
+};
+
+//INITIALIZE PLAYERS
+const player1 = playerFactory("Player 1", "X");
+const player2 = playerFactory("Player 2", "O");
+
+let currentPlayer = player1;
+
+//WINNING CONDITIONS MODULE
+const winningConditions = (() => {
+  const winCondition = [
+    //HORIZONTAL
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //VERTICAL
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    //DIAGONAL
+    [2, 4, 6],
+    [0, 4, 8],
+  ];
+
+  const checkForWinner = () => {
+    const board = gameBoard.getBoard();
+    for (let i = 0; i < winCondition.length; i++) {
+      const [a, b, c] = winCondition[i];
+      if (board[a] && board[a] == board[b] && board[a] === board[c]) {
+        return true;
+      }
+    }
+    return false;
+  };
+  return { checkForWinner };
+})();
+//EVENT LISTENERS
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    const tableId = cell.getAttribute("data-table-id");
+    if (gameBoard.updateBoard(tableId, currentPlayer.symbol)) {
+      cell.textContent = currentPlayer.symbol;
+      if (winningConditions.checkForWinner()) {
+        document.querySelector(
+          ".message"
+        ).textContent = `${currentPlayer.name} WINS!`;
+        // document.querySelector(".message").classList.remove("hidden");
+        return;
+      }
+      //SWITCH PLAYERS
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
   });
 });
 
-//4 SELECTION
-if (playing) {
+//RESET BUTTON EVENT LISTENER
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", () => {
+  gameBoard.resetBoard();
   cells.forEach((cell) => {
-    cell.addEventListener("click", () => {
-      const tableId = cell.getAttribute("data-table-id");
-      if (board[tableId] == "") {
-        board[tableId] = currentPlayer.symbol;
-        cell.textContent = currentPlayer.symbol;
-
-        //CHECK FOR WIINER
-        checkForWinner();
-        //CHECK FOR EMPTY BOARD
-        const allFilled = board.every((value) => value !== "");
-        if (allFilled) {
-          document.querySelector(".message").textContent = "CAT GAME!";
-          return;
-        }
-
-        console.log(board);
-
-        //SWITCH PLAYERS
-        currentPlayer == players[0]
-          ? (currentPlayer = players[1])
-          : (currentPlayer = players[0]);
-      }
-    });
+    cell.textContent = "";
   });
-}
+  document.querySelector(".message").textContent = "";
+  currentPlayer = player1;
+});
